44c384bb2eca97bebe5729e3ca66b75ebd5770ea
SAMIA1008 <100969272+SAMIA1008@users.noreply.github.com>
2022-03-21 01:07:21 +0100

    Create db.php

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$server="sql104.epizy.com";
+$username="epiz_31329726";
+$password="6AJZTqvkEq";
+$dbname="epiz_31329726_samia";
+$conn= mysql_connect(server,username,password,dbname);
+if(!$conn)
+{
+die("connection failed:.mysqli_connect_error());
+}
+?>
